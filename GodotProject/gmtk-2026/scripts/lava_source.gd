154f54c4185f43d861000c94c524e2f95cb4e57e
Christopher Major <chriscjor100@gmail.com>
2026-07-25 16:20:11 -0400

    lava implemented

--- GodotProject/gmtk-2026/scripts/lava_source.gd
+++ GodotProject/gmtk-2026/scripts/lava_source.gd
@@ -8,6 +8,14 @@ func _ready() -> void:
 		LevelManagerNode.StepUpdate.connect(OnStepUpdate)
 
 @onready var Raycast = $RayCast2D
+@onready var LavaFlowPrefab = preload("res://scenes/objects/lava_flow.tscn")
 
-func OnStepUpdate():
-	pass
+var RaycastPositionOffet = 1
+
+func OnStepUpdate(playerPosition: Vector2):
+	if !Raycast.is_colliding():
+		var LavaFlow : Node2D = LavaFlowPrefab.instantiate()
+		add_child(LavaFlow)
+		LavaFlow.position = Raycast.target_position
+		RaycastPositionOffet += 1
+		Raycast.target_position = Vector2(0, RaycastPositionOffet * Constants.TileSize)
