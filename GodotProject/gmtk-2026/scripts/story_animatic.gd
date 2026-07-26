2b6c97a6e43593c00691694a68f53604d0a6942e
Christopher Major <chriscjor100@gmail.com>
2026-07-26 05:56:07 -0400

    Story animatic tweak

--- GodotProject/gmtk-2026/scripts/story_animatic.gd
+++ GodotProject/gmtk-2026/scripts/story_animatic.gd
@@ -6,3 +6,10 @@ func _on_button_pressed() -> void:
 func _ready() -> void:
 	var MusicManagerReference = MusicManager as MusicManagerNode
 	MusicManagerReference.SwitchMusic("res://assets/audio/Music/Lose.mp3")
+
+@onready var ButtonNode = $Button
+@onready var ButtonLabelNode = $Button/Label
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	ButtonNode.visible = true
+	ButtonLabelNode.visible = true
