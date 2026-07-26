5f6986e47d2abc4d26bc7e0ede46fd4f85489081
Christopher Major <chriscjor100@gmail.com>
2026-07-26 05:19:45 -0400

    Timer improvements

--- GodotProject/gmtk-2026/scripts/step_timer.gd
+++ GodotProject/gmtk-2026/scripts/step_timer.gd
@@ -1,18 +1,27 @@
 extends Control
 class_name StepTimer
 
-var MaxFrames : int = 0
+var LastFrameIndex : int
 
-@onready var TimerAnimation = $AnimatedSprite2D
+@onready var TimerAnimation = $AnimatedSprite2DTimer
+@onready var FireAnimation = $AnimatedSprite2DFire
 @onready var LivesLabel = $LivesLabel
 
 func _ready() -> void:
-	MaxFrames = TimerAnimation.get_sprite_frames().get_frame_count("default")
+	LastFrameIndex = TimerAnimation.get_sprite_frames().get_frame_count("default") - 1
 
-func UpdateStepAmount(CurrentStepAmount : float, MaxStepAmount : float):
-	var NewFrame = int((CurrentStepAmount / MaxStepAmount) * MaxFrames)
-	NewFrame = MaxFrames - NewFrame
-	TimerAnimation.frame = NewFrame
+var FirePositions = [Vector2(78,-116), Vector2(95,-111), Vector2(104,-102), Vector2(108,-93), Vector2(110,-80), Vector2(111,-69), Vector2(105,-48), Vector2(95,-35), Vector2(67,-29), Vector2(40,-33), Vector2(27,-41), Vector2(20,-51), Vector2(20,-68), Vector2(22,-89), Vector2(28,-100), Vector2(43,-115), Vector2(65,-109)]
+
+func UpdateStepAmount(StepsRemaining, MaxStepAmount):
+	var FrameIndex = 0
+	if StepsRemaining == 1:
+		FrameIndex = LastFrameIndex
+	else:
+		var RemainingPercentage = float(StepsRemaining) / float(MaxStepAmount)
+		var RemainingFrames = RemainingPercentage * LastFrameIndex
+		FrameIndex = LastFrameIndex - RemainingFrames
+	TimerAnimation.frame = FrameIndex
+	FireAnimation.position = FirePositions[FrameIndex]
 
 func UpdateLivesAmount(LivesAmount):
 	LivesLabel.text = str(LivesAmount)
