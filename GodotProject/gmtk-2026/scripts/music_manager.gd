be5fa447bbb23db9bf4dd78f5955c063bafe236e
Christopher Major <chriscjor100@gmail.com>
2026-07-26 04:05:22 -0400

    Update music_manager.gd

--- GodotProject/gmtk-2026/scripts/music_manager.gd
+++ GodotProject/gmtk-2026/scripts/music_manager.gd
@@ -1,11 +1,12 @@
 extends Node2D
 class_name MusicManagerNode
 
-@onready var Music = $Music
+@onready var Music : AudioStreamPlayer = $Music
 
 func SwitchMusic(StreamString : String):
 	var AudioStreamReference = load(StreamString) as AudioStream
 	
-	Music.stop()
-	Music.set_stream(AudioStreamReference)
-	Music.play()
+	if Music.get_stream() != AudioStreamReference:
+		Music.stop()
+		Music.set_stream(AudioStreamReference)
+		Music.play()
